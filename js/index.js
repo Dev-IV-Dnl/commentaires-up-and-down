604902fdb192ebec08ee1986795e0fb1242cbe6a
Dev-IV-Dnl <mattgiordanengo@gmail.com>
2021-10-06 15:56:24 +0200

    maj btn pour changer de card : OK

--- js/index.js
+++ js/index.js
@@ -5,26 +5,31 @@ let allCards = [card1, card2, card3];
 let btn = document.querySelector(".btn");
 tabNum = [1, 2, 3];
 
-for(let i in allCards) {
-    allCards[i].addEventListener("click", () => {
-        console.log("hello !");
-        if(allCards[i]==card1) {
-            card1.style.display = "none";
-            card2.style.display = "flex";
-        } else if(allCards[i] == card2) {
-            card2.style.display = "none";
-            card3.style.display = "flex";
-        } else if(allCards[i] == card3) {
-            card3.style.display = "none";
-            card1.style.display = "flex";
-        }
-    })
-}
-
-// btn.addEventListener("click", () => {
-//     console.log("Je clique sur mon btn")
-//     if(card1.style.display == "flex") {
+// for(let i in allCards) {
+//     allCards[i].addEventListener("click", () => {
+//         if(allCards[i]==card1) {
 //             card1.style.display = "none";
 //             card2.style.display = "flex";
-//     }
-// })
+//         } else if(allCards[i] == card2) {
+//             card2.style.display = "none";
+//             card3.style.display = "flex";
+//         } else if(allCards[i] == card3) {
+//             card3.style.display = "none";
+//             card1.style.display = "flex";
+//         }
+//     })
+// }
+
+btn.addEventListener("click", () => {
+    console.log("Je clique sur mon btn");
+    if(card1.style.display == "flex") {
+            card1.style.display = "none";
+            card2.style.display = "flex";
+    } else if (card2.style.display == "flex") {
+        card2.style.display = "none";
+        card3.style.display = "flex";
+    } else if (card3.style.display == "flex") {
+        card3.style.display = "none";
+        card1.style.display = "flex";
+    }
+})
